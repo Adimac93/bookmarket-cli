12bb119d83e436396cc21bf1d2f4d0158ea8248c
Adimac93 <Adimac93@gmail.com>
2022-07-03 23:08:25 +0200

    cli: huge remake
    +fetched book edit
    
    ps: I've got in trouble with npm modules for too long

--- src/cli.ts
+++ src/cli.ts
@@ -1,75 +1,178 @@
-import readLine from "readline";
-import { getBook } from "./lib/loader";
-import { subjects } from "./lib/common";
-import { stdin, stdout } from "node:process";
-import fs from "node:fs/promises";
-import { Book } from "./lib/common";
-const rl = readLine.createInterface({ input: stdin, output: stdout });
-
+import { fetchBook } from "./lib/loader";
+import { testUrlSet } from "./lib/common";
+import * as fs from "node:fs/promises";
+import { increment } from "./lib/common";
+import { Book, Grade, Subject } from "@prisma/client";
+import { uploadBook } from "./lib/database";
+import { prompt, registerPrompt } from "inquirer";
+registerPrompt("search-list", require("inquirer-search-list"));
 const fileName = "./data.json";
+
+const booksMap = new Map<string, number>();
+Object.entries(Subject).map(([k, subject]) => {
+    booksMap.set(subject, 0);
+});
 async function main() {
+    const data = await loadFile();
+
+    const books = await promptAddBooks();
+
+    //await fs.writeFile(fileName, JSON.stringify(books, null, 4), "utf-8");
+}
+const loadFile = async () => {
     const file = await fs.readFile(fileName, { flag: "a+", encoding: "utf-8" });
     let data = {};
 
     try {
         data = JSON.parse(file);
-        console.log(data);
+        //console.log(data);
     } catch (err) {
         console.error(`Failed to parse "${fileName}"`);
         if (await confirm("Clear?")) {
             await fs.writeFile(fileName, "{}", "utf-8");
         }
+    } finally {
+        return data;
     }
+};
+async function promptAddBooks() {
+    const books: Book[] = [];
+    let isNext = true;
+    while (isNext) {
+        const options = await prompt({
+            name: "url",
+            message: "Url",
+            type: "input",
 
-    const books = await addBooks();
-    await fs.writeFile(fileName, JSON.stringify(books, null, 4), "utf-8");
-    rl.close();
+            validate: (url) => /(https:\/{2}w{3}.taniaksiazka.pl\/.*\.html)/i.test(url) || "Invlaid url",
+        });
+        const book = await getBook(options.url);
+        if (!book) return books;
+
+        const fill = await prompt([
+            {
+                name: "mode",
+                message: "Mode",
+                type: "list",
+                choices: ["accept", "edit", "guided-edit"],
+            },
+            {
+                name: "title",
+                message: "Title",
+                default: book.title || "brak tytułu",
+                type: "input",
+                when: (fill) => {
+                    if (book.title && fill.mode != "edit") {
+                        return false;
+                    }
+                    return true;
+                },
+            },
+            {
+                name: "authors",
+                message: "Authors",
+                default: book.author || "brak autora",
+                type: "input",
+                when: (fill) => {
+                    if (book.author && fill.mode != "edit") {
+                        return false;
+                    }
+                    return true;
+                },
+                filter: (authors: string) => {
+                    return authors
+                        .split(",")
+                        .map((author) => {
+                            const match = /([\w-]+\s?)+/giu.exec(author.trim());
+                            if (match) {
+                                return match[0];
+                            }
+                            return author.trim();
+                        })
+                        .join(", ");
+                },
+            },
+            {
+                name: "grade",
+                message: "Grade",
+                type: "list",
+                default: book.grade,
+                choices: Object.entries(Grade).map(([k, v]) => k),
+                when: (fill) => {
+                    if (book.grade && fill.mode != "edit") {
+                        return false;
+                    }
+                    return true;
+                },
+            },
+            {
+                name: "subject",
+                message: "Subject",
+                type: "search-list",
+                default: book.subject,
+                choices: Object.entries(Subject).map(([k, v]) => k),
+                when: (fill) => {
+                    if (book.subject && fill.mode != "edit") {
+                        return false;
+                    }
+                    return true;
+                },
+            },
+            {
+                name: "is_advanced",
+                message: "Is advanced",
+                type: "confirm",
+                when: (fill) => {
+                    if (book.is_advanced == undefined && fill.mode != "edit") {
+                        return false;
+                    }
+                    return true;
+                },
+            },
+            // {
+            //     name: "image",
+            //     message: "Image",
+            //     type: "input",
+            //     default: book.image,
+            // },
+            // {
+            //     name: "id",
+            //     message: "ISBN",
+            //     type: "input",
+            //     default: book.id,
+            // },
+            // {
+            //     name: "price",
+            //     message: "Price",
+            //     type: "number",
+            //     default: book.price,
+            // },
+        ]);
+        if (fill.title) {
+            book.title = fill.title;
+        }
+        if (fill.authors) {
+            book.author = fill.authors;
+        }
+        if (fill.grade) {
+            book.grade = fill.grade;
+        }
+        if (fill.subject) {
+            book.subject = fill.subject;
+        }
+        if (fill.is_advanced) {
+            book.is_advanced = fill.is_advanced;
+        }
+        console.log(book);
+        isNext = false;
+    }
 }
 
-const addBooks = () =>
-    new Promise<Book[]>(async (resolve, reject) => {
-        const books: Book[] = [];
-        let isNext = true;
-        while (isNext) {
-            try {
-                const url = await askURL();
-                books.push(await getBook(url));
-            } catch (err) {
-                console.log(err);
-            }
-
-            isNext = await confirm("Next?");
-        }
-        if (!isNext) {
-            resolve(books);
-        }
-    });
-
-const askURL = () =>
-    new Promise<string>((resolve, reject) => {
-        rl.question("URL: ", async (url) => {
-            const match = url.match(
-                /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/i
-            );
-            if (match) {
-                resolve(match[1]);
-            }
-            reject("Cannot parse URL");
-        });
-    });
-
-const confirm = (prompt: string) =>
-    new Promise<boolean>((resolve, reject) => {
-        rl.question(`${prompt} (y\\n) `, async (res) => {
-            if (!res) {
-                resolve(true);
-            }
-            const match = /(y|n|yes|no)/.exec(res);
-            if (match) {
-                resolve(match[1] == "y");
-            }
-            resolve(false);
-        });
-    });
-
 main();
+const getBook = async (url: string) => {
+    try {
+        return await fetchBook(/(https:\/{2}w{3}.taniaksiazka.pl\/.*\.html)/i.exec(url)![1]);
+    } catch (err) {
+        console.log((err as Error).message);
+    }
+};
