9e62140f726898518424135833e330399414f7dd
adimac93 <adimac93@gmail.com>
2022-07-11 08:37:55 +0200

    cli: add simple program loop

--- src/cli.ts
+++ src/cli.ts
@@ -2,24 +2,30 @@ import { promptEditSchema } from './lib/editor';
 import { promptFetchBook, promptSearchBooks } from './lib/search';
 
 import { prompt } from 'inquirer';
+import { promptViewBooks } from './lib/view';
 
 async function main() {
-	const options = await prompt({
-		name: 'menu',
-		message: 'Choose menu',
-		choices: [
-			{ name: 'schema editor', value: 'schema' },
-			{ name: 'book loader', value: 'books' },
-			{ name: 'book browser', value: 'search' },
-		],
-		type: 'list',
-	});
-	if (options.menu == 'schema') {
-		await promptEditSchema();
-	} else if (options.menu == 'books') {
-		await promptFetchBook();
-	} else if (options.menu == 'search') {
-		await promptSearchBooks();
+	while (true) {
+		const options = await prompt({
+			name: 'menu',
+			message: 'Choose menu',
+			choices: [
+				{ name: 'schema editor', value: 'schema' },
+				{ name: 'book loader', value: 'books' },
+				{ name: 'book browser', value: 'search' },
+				{ name: 'exit and save', value: 'exit' },
+			],
+			type: 'list',
+		});
+		if (options.menu == 'schema') {
+			await promptEditSchema();
+		} else if (options.menu == 'books') {
+			await promptFetchBook();
+		} else if (options.menu == 'search') {
+			await promptSearchBooks();
+		} else if (options.menu == 'exit') {
+			break;
+		}
 	}
 }
 
